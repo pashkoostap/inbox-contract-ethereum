729f22cc8f1664dc9d3f438dc162bc20d9cc8eaf
pashkoostap <pashko.ostap@gmail.com>
2018-06-24 21:03:12 +0300

    added solc module to the project; updated compile.js module;

---
+++ compile.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const InboxContractPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(InboxContractPath, 'utf8');
+const compiledCode = solc.compile(source, 1);
+const ImboxContract = compiledCode.contracts[':Inbox'];
+
+module.exports = ImboxContract;
