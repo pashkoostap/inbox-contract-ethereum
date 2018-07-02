5340fd972fe71bd32d634dbc4d66285ba2670cf6
pashkoostap <pashko.ostap@gmail.com>
2018-07-02 23:46:11 +0300

    updated Inbox.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,4 +4,15 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {});
+beforeEach(() => {
+  // Get a list of all accounts
+  const accounts = web3.eth.getAccounts().then(accounts => {
+    console.log('accounts', accounts);
+  });
+
+  // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+  it('should log the accounts', () => {});
+});
