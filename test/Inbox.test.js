a04af2b3c116c9c56628ec16f651b3d217bd609c
pashkoostap <pashko.ostap@gmail.com>
2018-07-03 21:47:11 +0300

    updated Inbox.test.js with checking defaultMessage

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,6 +9,7 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
 }
 const { interface, bytecode } = require('../compile');
+const defaultMessage = 'Hello World!';
 let accounts = [];
 let inbox;
 
@@ -20,7 +21,7 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ['Hello World!']
+      arguments: [defaultMessage]
     })
     .send({ from: accounts[0], gas: '1000000' });
 });
@@ -28,5 +29,11 @@ beforeEach(async () => {
 describe('Inbox', () => {
   it('should deploy the contract', () => {
     assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+
+    assert.equal(message, defaultMessage);
   });
 });
